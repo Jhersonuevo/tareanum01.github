86f6dab0c0445b55f86cf7ba02ce0a893c7a4bce
Jhersonuevo <apazajherson8@gmail.com>
2025-04-09 22:52:00 -0500

    Add files via upload

---
+++ NEW_PROJECT/main.js
@@ -0,0 +1,16 @@
+noBtn.addEventListener("mouseover", () = {
+    const newY = Math.gfloor(Math.ramdon) * 
+    questionContainer,offsetWidth);
+    const newY = Math.floor(Math.random() *
+    question.questionContainer.offsetWidth);
+
+    noBtn.style.left = '${newX}px';
+    noBtn.style.top = '${newY}px';
+}):
+yesBtn.addEventListener("click", () = {
+    const timeoutId = setTimeout(() = {
+        heartLoder.style.display = "none";
+        resultContainer.style.display = "none";
+        gifResult.play();
+    }, 3000);
+});
